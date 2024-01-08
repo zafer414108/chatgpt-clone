780ddb198d23d7121b73502c0523fc36e7a67511
zafer414108 <kczafer4111@gmail.com>
2024-01-08 21:38:59 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+/* tarayıcıların depolama alanları localstorage ve sessiovsstorage, tarayıcının sunduğu iki farklı web depolama alanı*/
+
+//localStorage'a veri ekleme
+localStorage.setItem("chat", "veriekleme");
+
+//localStorage'dan veri çekme
+localStorage.getItem("chat")
+
+console.log(chat)
